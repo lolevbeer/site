a5809537fb5137568535a4cfaffe7d0379dfff71
Ted Slesinski <ted@slesinski.org>
2023-09-15 13:00:42 -0400

    Add white backgrounds

--- assets/beer/resize-images.sh
+++ assets/beer/resize-images.sh
@@ -3,4 +3,5 @@
 for file in *.png; do
   cwebp -q 100 -resize 386 405  -crop 330 300 1930 2025 "$file" -o "$(basename "$file" .png).webp"
   cwebp -q 100 -resize 38 40  -crop 330 300 1930 2025 "$file" -o "$(basename "$file" .png)-small.webp"
+  convert "$file" -background white -alpha remove -alpha off "$(basename "$file" .png)-white.png"
 done
