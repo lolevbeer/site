1292f3ca3558495e1f2a14715c7b40ac27bf3dab
Ted Slesinski <ted@slesinski.org>
2023-10-16 11:34:24 -0400

    Add matcha and drip

--- assets/beer/resize-images.sh
+++ assets/beer/resize-images.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 for file in *.png; do
+  # Check if the file name matches *-white.png and skip processing if it does
+  if [[ "$file" == *"-white.png" ]]; then
+    continue
+  fi
+
   cwebp -q 100 -resize 386 405  -crop 330 300 1930 2025 "$file" -o "$(basename "$file" .png).webp"
   cwebp -q 100 -resize 38 40  -crop 330 300 1930 2025 "$file" -o "$(basename "$file" .png)-small.webp"
   convert "$file" -background white -alpha remove -alpha off "$(basename "$file" .png)-white.png"
