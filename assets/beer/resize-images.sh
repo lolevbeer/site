34c1ec262cc034e9de74ad6199c37b4169d93467
Ted Slesinski <ted@slesinski.org>
2024-01-10 00:24:27 -0500

    Update resize image script and add double lupe and prismatic

--- assets/beer/resize-images.sh
+++ assets/beer/resize-images.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-for file in *.png; do
-  # Check if the file name matches *-white.png and skip processing if it does
+# This script resizes and converts PNG images to WebP format.
+# It skips processing if the file name matches *-white.png.
+# The script uses the cwebp tool to convert images to WebP format and the convert tool to add a white background to images.
+# The resized images are saved with the same name as the original file, but with the .webp or -small.webp suffix.
+# The images are resized to specific dimensions and cropped to specific coordinates before conversion.
+
+shopt -s nullglob
+
+for file in *-*\.png *.png; do
   if [[ "$file" == *"-white.png" ]]; then
     continue
   fi
 
-  cwebp -q 100 -resize 386 405  -crop 330 300 1930 2025 "$file" -o "$(basename "$file" .png).webp"
-  cwebp -q 100 -resize 38 40  -crop 330 300 1930 2025 "$file" -o "$(basename "$file" .png)-small.webp"
-  convert "$file" -background white -alpha remove -alpha off "$(basename "$file" .png)-white.png"
+  if [[ ! -f "$(basename "$file" .png).webp" ]]; then
+    cwebp -q 100 -resize 386 405  -crop 330 300 1930 2025 "$file" -o "$(basename "$file" .png).webp"
+  fi
+
+  if [[ ! -f "$(basename "$file" .png)-small.webp" ]]; then
+    cwebp -q 100 -resize 38 40  -crop 330 300 1930 2025 "$file" -o "$(basename "$file" .png)-small.webp"
+  fi
+
+  if [[ ! -f "$(basename "$file" .png)-white.png" ]]; then
+    convert "$file" -background white -alpha remove -alpha off "$(basename "$file" .png)-white.png"
+  fi
 done
