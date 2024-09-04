84bb79eeb42fccdabdd59420688d8a3424869ce5
Ted Slesinski <ted@slesinski.org>
2024-09-04 13:46:20 -0400

    Fix last updated to build on page load

--- _plugins/last_updated.rb
+++ _plugins/last_updated.rb
@@ -1,4 +1,12 @@
+require 'date'
+
 Jekyll::Hooks.register :site, :pre_render do |site|
-  site.data['last_updated'] = `git log -1 --format="%ar"`.strip
+  # Get the last commit time from the git log
+  last_commit_time = `git log -1 --format="%at"`.strip.to_i
+  # Convert the timestamp to a readable format
+  last_updated_time = Time.at(last_commit_time).utc.iso8601
+
+  # Store the last updated time in the site data
+  site.data['last_updated'] = last_updated_time
   puts "Last updated time: #{site.data['last_updated']}"
 end
