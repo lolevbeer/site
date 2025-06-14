d0caf1eb1018cb8bb9a0edb92aedc3b329d725e1
energee <energee@users.noreply.github.com>
2025-06-14 15:59:23 +0000

    deploy: 8ce261ff624c33bce218caf1d96535efea36ec99

---
+++ assets/auth.js
@@ -0,0 +1,58 @@
+new Vue({
+    el: '#app',
+    delimiters: ['${', '}'],
+    data() {
+        return {
+            inventory: [],
+            password: localStorage.getItem('password') || ''
+        };
+    },
+    async mounted() {
+        if (this.password) {
+            await this.loadInventory();
+        } else {
+            this.promptPassword();
+        }
+    },
+    methods: {
+        promptPassword() {
+            const password = localStorage.getItem('password');
+            if (password) {
+                this.password = password;
+                this.loadInventory();
+            } else {
+                mscPrompt('Enter password', (enteredPassword) => {
+                    this.password = enteredPassword;
+                    localStorage.setItem('password', enteredPassword);
+                    this.loadInventory();
+                });
+            }
+        },
+        async loadInventory() {
+            try {
+                const response = await axios.get(`https://docs.google.com/spreadsheets/d/e/2PACX-1vQIcKPeDcvTKS_7zH7-XekKAuO3cl7juVJOy3upThDIM2nnca7WKrXpYIC8oBebXDMM35hstr${this.password}/pub?gid=0&single=true&output=csv`);
+                const parsedData = Papa.parse(response.data, { header: true }).data;
+                this.inventory = parsedData;
+                console.log(parsedData)
+                this.$nextTick(() => {
+                    this.addEventListeners(); // Bind event listeners after setting inventory data
+                });
+            } catch (error) {
+                mscAlert('Incorrect');
+                localStorage.removeItem('password');
+                this.promptPassword();
+            }
+        },
+        addEventListeners() {
+            const rowControlElements = document.querySelectorAll('.row-control');
+            rowControlElements.forEach(element => {
+                element.addEventListener('click', this.handleRowControlClick);
+            });
+        },
+        handleRowControlClick(event) {
+            console.log("clicked");
+            const tr = event.currentTarget.closest('tr');
+            tr.classList.toggle('open');
+        }
+    }
+});
