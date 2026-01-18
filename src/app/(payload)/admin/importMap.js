553b884db09717638296b76b8396d076e4e04995
Ted Slesinski <ted@slesinski.org>
2026-01-18 14:36:09 -0500

    Rebuild import map

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -10,6 +10,7 @@ import { AdminNavLink as AdminNavLink_718cb223f4c9ff40cc70f8d31e60b60b } from '.
 import { LinesCleanedAlert as LinesCleanedAlert_ea4d0d6c304332acbeb3a86b04f0d005 } from '../../../components/admin/LinesCleanedAlert'
 import { VercelBlobClientUploadHandler as VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e } from '@payloadcms/storage-vercel-blob/client'
 import { SyncView as SyncView_c23504be653ff4cfe56b71cd946ad683 } from '../../../components/SyncView'
+import { CollectionCards as CollectionCards_ab83ff7e88da8d3530831f296ec4756a } from '@payloadcms/ui/rsc'
 
 export const importMap = {
   "@/src/components/admin/UntappdFetcher#UntappdFetcher": UntappdFetcher_98d9a61c943279a91d72840260fe5c0b,
@@ -23,5 +24,6 @@ export const importMap = {
   "./components/AdminNavLink#AdminNavLink": AdminNavLink_718cb223f4c9ff40cc70f8d31e60b60b,
   "./components/admin/LinesCleanedAlert#LinesCleanedAlert": LinesCleanedAlert_ea4d0d6c304332acbeb3a86b04f0d005,
   "@payloadcms/storage-vercel-blob/client#VercelBlobClientUploadHandler": VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e,
-  "./components/SyncView#SyncView": SyncView_c23504be653ff4cfe56b71cd946ad683
+  "./components/SyncView#SyncView": SyncView_c23504be653ff4cfe56b71cd946ad683,
+  "@payloadcms/ui/rsc#CollectionCards": CollectionCards_ab83ff7e88da8d3530831f296ec4756a
 }
